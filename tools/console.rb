39e9b5fd429a02115617d7cf8eb43618427fc169
liam <leamsi.esc@gmail.com>
2020-06-07 18:58:30 -0400

    Working on iMDB

--- tools/console.rb
+++ tools/console.rb
@@ -4,15 +4,6 @@ def reload
   load 'config/environment.rb'
 end
 
-mompops = Bakery.new("Mom & Pops")
-
-cupcake = Dessert.new(mompops, "Cupcake")
-cupcake = Dessert.new(mompops, "Cupcake")
-cupcake = Dessert.new(mompops, "Cupcake")
-
-chocolate = Ingredient.new("Chocolate", 120, cupcake)
-salt = Ingredient.new("Salt", 20, cupcake)
-sugar = Ingredient.new("Sugar", 30, cupcake)
 
 
 Pry.start
